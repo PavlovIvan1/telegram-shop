7946d8f968dbea4e82c70b5888acb321209ea05d
PavlovIvan1 <pavlov-2011pavlov@yandex.ru>
2025-08-05 21:30:50 +0300

    commit: added first card

--- src/components/section/Products/Products.tsx
+++ src/components/section/Products/Products.tsx
@@ -3,7 +3,8 @@ import { ProductCard } from '../../ProductCard/ProductCard'
 export function Products() {
 	return (
 		<>
-			<div>
+			<div style={{ display: 'flex', gap: '10px' }}>
+				<ProductCard />
 				<ProductCard />
 			</div>
 		</>
