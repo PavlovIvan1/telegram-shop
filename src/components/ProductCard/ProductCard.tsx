27329b3465b99a54d43e12eb560dc3ac6c78ffff
PavlovIvan1 <pavlov-2011pavlov@yandex.ru>
2025-08-05 21:56:58 +0300

    commit: added first card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -7,7 +7,7 @@ export function ProductCard() {
 		<>
 			<div className={styles.card}>
 				<img src='/products/iphone-12-green.png' alt='' />
-				<div>
+				<div className={styles.card_info}>
 					<h3>Iphone 12</h3>
 					<span>124 000 р.</span>
 				</div>
